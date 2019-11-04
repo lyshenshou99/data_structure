82e9b219da78c93ed94ba960c0382912b1084fb2
lyshenshou99 <55307611+lyshenshou99@users.noreply.github.com>
2019-11-04 21:09:51 +0800

    Update dfs.c

--- 第6章/dfs.c
+++ 第6章/dfs.c
@@ -1,28 +1,35 @@
 #include<stdio.h>
 #include<string.h>
 
-int a[] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //给出的数组
+#define bool int
+#define true  1
+#define false 0
+
+int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //给出的数组
 int size = sizeof(a) / sizeof(int);
-int sum = 10, k = 3;      //计算的和 要求的个数
+int sum = 23, k = 3;      //计算的和 要求的个数
+int i = 0;
 
 void Print(bool* x)
 {
-    for(int i = 0; i < size; i++) 
+    for(i = 0; i < size; i++) 
 	{
         if(x[i] == 1)
 		{
-			printf("%d", x[i]);
+			printf("%-4d", a[i]);
 		}
     }
+    printf("\n");
 }
 
-//x[]为最终解，i为考察第x[i]是否加入，has表示当前的和
+//x[]为最终解，i为考察a[i]是否加入，has表示当前的和
 void EnumNumber(bool* x, int i, int has, int step) 
 {
-    if(i >= size) return;
-	if(step >= k) return;
+    if(step > k) return;
+	if(i >= size) return;
+	if(has + a[i] > sum) return;
 	
-    if(has + a[i] == sum && step == k) 
+    if((has + a[i] == sum) && (step == k)) 
 	{
         x[i] = true;
         Print(x);
@@ -33,12 +40,12 @@ void EnumNumber(bool* x, int i, int has, int step)
     EnumNumber(x, i + 1, has + a[i], step + 1);
 	
     x[i] = false;
-    EnumNumber(x, i + 1, has, step + 1);
+    EnumNumber(x, i + 1, has, step);
 }
 
 int main() 
 {
-    bool x[size] = {};
+    bool x[size];
 	
     memset(x, 0, size);
 
